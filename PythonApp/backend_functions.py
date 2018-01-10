aa1c703ca9b77e454cae0c58854b4202740e283d
NJ4r0 <236495@student.pwr.edu.pl>
2018-01-10 23:46:23 +0100

    added simple possibility to execute sql commands

--- PythonApp/backend_functions.py
+++ PythonApp/backend_functions.py
@@ -6,10 +6,11 @@ import pymysql as mariadb
 
 def database_connect(dbhost, dbuser, dbpasswd):
     """
-    this function connects to mariadb server.
-    dbhost is name or IP Address of the server (usually localhost)
-    dbuser is name of user in the database server (not the App user!)
-    dbpasswd is password of that user
+    connects to database. you can store the return to some variable to not repeat that
+    :param dbhost: name or IP of mariadb host
+    :param dbuser: mariadb username
+    :param dbpasswd: password of username
+    :return: returns Connection class
     """
     conn = mariadb.connect(
         host=dbhost,
@@ -19,5 +20,23 @@ def database_connect(dbhost, dbuser, dbpasswd):
         charset='utf8'
     )
     return conn
+
+
+def execute_sql_command(connection, statement): #Warning! that is insecure and musn't be used as GUI function!
+    """
+    executes any sql command
+    :param connection: Connection class on wich query will operate
+    :param statement: String to execute sql query
+    :return: void
+    """
+    isinstance(connection, mariadb.Connection)
+    cursor = connection.cursor()
+    try:
+        cursor.execute(statement)
+        connection.commit()
+    except Exception as e:
+        connection.rollback()
+        print("something went wrong")
+
 
 
