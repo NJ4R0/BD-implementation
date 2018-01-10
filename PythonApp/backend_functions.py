b5d1eb92204b9c40f5c2e8df3c87b4c6345b8275
NJ4r0 <236495@student.pwr.edu.pl>
2018-01-10 23:23:08 +0100

    forgot about usage of the function added

--- PythonApp/backend_functions.py
+++ PythonApp/backend_functions.py
@@ -5,6 +5,12 @@ import pymysql as mariadb
 
 
 def database_connect(dbhost, dbuser, dbpasswd):
+    """
+    this function connects to mariadb server.
+    dbhost is name or IP Address of the server (usually localhost)
+    dbuser is name of user in the database server (not the App user!)
+    dbpasswd is password of that user
+    """
     conn = mariadb.connect(
         host=dbhost,
         user=dbuser,
